2c6409f6e153a7f7481c77d714a1861db250bec5
tara-nguyen <70169207+tara-nguyen@users.noreply.github.com>
2023-07-12 11:42:26 +0700

    Edit gov1_council_motions.py

--- governance/subsquare/gov1_council_motions.py
+++ governance/subsquare/gov1_council_motions.py
@@ -6,7 +6,7 @@ def get_data(network):
     fields = ["motionIndex", "state", "proposer", "title", "content",
               "createdAt", "onchainData_threshold", "onchainData_voting_ayes",
               "onchainData_voting_nays", "onchainData_state_indexer_blockTime",
-              "commentsCount", "polkassemblyCommentsCount", "referendumIndex",
+              "commentsCount", "polkassemblyCommentsCount",
               "onchainData_treasuryBounties", "onchainData_treasuryProposals",
               "onchainData_externalProposals"]
     time_cols = ["onchainData_state_indexer_blockTime", "createdAt"]
@@ -15,16 +15,18 @@ def get_data(network):
     df = Transformer(data).transform(fields, time_cols=time_cols)
     df = df.dropna(subset="motionIndex")
 
+    new_cols = fields[:5]
+    new_cols += ["submissionTime", "approvalThreshold", "ayeVoters",
+                 "nayVoters", "lastUpdateTime", "subsquareCommentCount",
+                 "polkassemblyCommentCount", "treasuryBounties",
+                 "treasuryProposals", "externalProposals"]
+    df = df.set_axis(new_cols, axis=1)
+
     return df
 
 
 if __name__ == "__main__":
     motions = get_data("kusama")
-    # df = pr.trim_long_strings(motions, ["title", "content"])
-    # df = pr.trim_account_id_strings(df, ["proposer"])
-    # pr.print_long_df(df)
-
-    df = motions.reindex(columns=["onchainData_treasuryBounties",
-                                  "onchainData_treasuryProposals",
-                                  "onchainData_externalProposals"])
-    print(df.to_string())
+    df = pr.trim_long_strings(motions, ["title", "content"])
+    df = pr.trim_account_id_strings(df, ["proposer"])
+    pr.print_long_df(df)
