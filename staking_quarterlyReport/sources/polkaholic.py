5ead9d62ce012511c1c3a9748929e1ab701215ab
tara-nguyen <70169207+tara-nguyen@users.noreply.github.com>
2023-07-28 17:30:43 +0700

    Edit source scripts for staking data

--- staking_quarterlyReport/sources/polkaholic.py
+++ staking_quarterlyReport/sources/polkaholic.py
@@ -1,42 +1,20 @@
-import pandas as pd
 from google.cloud import bigquery
 from google.oauth2 import service_account
-from reports.quarterly_etl import QuarterlyReport, extract
+from reports.quarterly_etl import QuarterlyReport
 
 
 class PolkaholicExtractor:
-    def __init__(self, from_bigquery=True, credentials="service-account.json",
-                 route=""):
-        self.from_bigquery = from_bigquery
-        if from_bigquery:
-            credentials = service_account.Credentials.from_service_account_file(
-                credentials)
-            self.client = bigquery.Client(credentials=credentials)
-            self.timestamp_format = "%Y-%m-%d"
-        else:
-            self.method = "GET"
-            self.url = f"https://api.polkaholic.io{route}"
+    def __init__(self, credentials="service-account.json"):
+        credentials = service_account.Credentials.from_service_account_file(
+            credentials)
+        self.client = bigquery.Client(credentials=credentials)
 
-    def extract_bigquery(self, query_template,
-                         start=QuarterlyReport().start_time,
-                         end=QuarterlyReport().end_time, **kwargs):
-        """Extract data from Polkaholic."""
-        start, end = [t.strftime(self.timestamp_format) for t in [start, end]]
+    def extract(self, query_template, start=QuarterlyReport().start_time,
+                end=QuarterlyReport().end_time, **kwargs):
+        """Extract data from Polkaholic's Big Query dataset."""
+        start, end = [t.strftime("%Y-%m-%d") for t in [start, end]]
         query = query_template.substitute(start=start, end=end, **kwargs)
         data = self.client.query(query)
-
-        return data
-
-    def extract_api(self, params):
-        data = extract(self.method, self.url, params=params)
-
-        return data
-
-    def extract(self, *args, **kwargs):
-        if self.from_bigquery:
-            data = self.extract_bigquery(*args, **kwargs)
-        else:
-            data = self.extract_api(*args)
 
         return data
 
@@ -46,9 +24,6 @@ class PolkaholicTransformer:
         self.data = data
 
     def to_frame(self):
-        if isinstance(self.data, bigquery.QueryJob):
-            df = self.data.to_dataframe()
-        else:
-            df = pd.json_normalize(self.data, sep="_")
+        df = self.data.to_dataframe()
 
         return df
--- staking_quarterlyReport/sources/subscan.py
+++ staking_quarterlyReport/sources/subscan.py
@@ -3,22 +3,11 @@ from time import sleep
 
 
 class SubscanExtractor:
-    def __init__(self, route, api_version=1, network="Polkadot", chain=None):
+    def __init__(self, path_end, api_version=1):
         self.method = "POST"
-
-        if network in ["Polkadot", "Kusama"]:
-            if chain is None:
-                chain = network
-        else:
-            raise Exception('network must be either "Polkadot" or "Kusama" '
-                            '(case-sensitive)')
-        networks = SubscanNetworks().get_network(network)
-        if chain in networks:
-            self.url = f"https://{networks[chain][0]}.api.subscan.io/api/"
-            self.url += "scan" if api_version == 1 else "v2/scan"
-            self.url += route
-        else:
-            raise Exception(f"{chain} is not a parachain of {network}")
+        self.url = "https://polkadot.api.subscan.io/api/"
+        self.url += "scan" if api_version == 1 else "v2/scan"
+        self.url += path_end
 
     def extract(self, payload):
         """Extract data from Subscan."""
@@ -32,66 +21,3 @@ class SubscanExtractor:
                 break
 
         return data
-
-
-class SubscanNetworks:
-    def __init__(self):
-        self.main_parachains = ["polkadot", "kusama"]
-
-    def get_network(self, relaychain):
-        relaychain = relaychain.lower()
-        if relaychain == "polkadot":
-            networks = {"Polkadot": ["polkadot", 1e10],
-                        "AssetHub-Polkadot": ["assethub-polkadot", 1e10],
-                        "Acala": ["acala", 1e12],
-                        "Astar": ["astar", 1e18],
-                        "Bifrost": ["bifrost", 1e13],
-                        "Centrifuge": ["centrifuge", 1e18],
-                        "Clover Parachain": ["clv", 1e18],
-                        "Composable": ["composable", 1e12],
-                        "Crust Parachain": ["crust-parachain", 1e12],
-                        "Darwinia2": ["darwinia", 1e18],
-                        "Equilibrium": ["equilibrium", 1e9],
-                        "HydraDX": ["hydradx", 1e12],
-                        "Interlay": ["interlay", 1e10],
-                        "KILT Spiritnet": ["spiritnet", 1e15],
-                        "Manta": ["manta", 1e18],
-                        "Moonbeam": ["moonbeam", 1e18],
-                        "Nodle": ["nodle", 1e11],
-                        "OriginTrail Parachain": ["origintrail", 1e12],
-                        "Parallel": ["parallel", 1e12],
-                        "Phala": ["phala", 1e12],
-                        "Unique": ["unique", 1e18],
-                        "Zeitgeist": ["zeitgeist", 1e10]}
-        elif relaychain == "kusama":
-            networks = {"Kusama": ["kusama", 1e12],
-                        "AssetHub-Kusama": ["assethub-kusama", 1e12],
-                        "Altair": ["altair", 1e18],
-                        "Bajun": ["bajun", 1e12],
-                        "Basilisk": ["basilisk", 1e12],
-                        "Bifrost Kusama": ["bifrost-kusama", 1e12],
-                        "Calamari": ["calamari", 1e12],
-                        "Crab2": ["crab", 1e18],
-                        "Datahighway Tanganika": ["datahighway", 1e18],
-                        "Encointer": ["encointer", 1e12],
-                        "Genshiro": ["genshiro", 1e9],
-                        "Parallel Heiko": ["parallel-heiko", 1e12],
-                        "IntegriTEE": ["integritee", 1e12],
-                        "Karura": ["karura", 1e12],
-                        "Khala": ["khala", 1e12],
-                        "Kintsugi": ["kintsugi", 1e12],
-                        "Mangata": ["mangatax", 1e18],
-                        "Moonriver": ["moonriver", 1e18],
-                        "Picasso": ["picasso", 1e12],
-                        "Pioneer": ["pioneer", 1e18],
-                        "Quartz": ["quartz", 1e18],
-                        "Robonomics": ["robonomics", 1e18],
-                        "Crust Shadow": ["shadow", 1e12],
-                        "Shiden": ["shiden", 1e18],
-                        "Turing": ["turing", 1e10]}
-        else:
-            raise Exception(
-                f'The main parachain must be either {self.main_parachains[0]} '
-                f'or {self.main_parachains[1]}')
-
-        return networks
