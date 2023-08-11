3c95fb836116a87fee1cfd12118a71345f6e68c1
tara-nguyen <70169207+tara-nguyen@users.noreply.github.com>
2023-08-11 15:18:00 +0700

    Revise source script for OpenSquare

--- sources/opensquare.py
+++ sources/opensquare.py
@@ -3,6 +3,8 @@ from reports.quarterly_etl import QuarterlyReport, extract, get_token_amount
 
 
 class OpensquareExtractor:
+    """Extract data from either SubSquare API or doTreasury API."""
+
     def __init__(self, source, network, route):
         self.method = "GET"
 
@@ -21,7 +23,8 @@ class OpensquareExtractor:
                 f'network must be either "{networks[0]}" or "{networks[1]}"')
 
     def extract(self):
-        """Extract data from either SubSquare or doTreasury."""
+        # The first extraction only returns the first page of data. If there is
+        # more than one page, extract everything
         data = extract(self.method, self.url)
         if "pageSize" in data:
             data = extract(self.method, self.url,
@@ -31,18 +34,33 @@ class OpensquareExtractor:
 
 
 class OpensquareTransformer:
+    """Convert json-encoded content to a dataframe."""
+
     def __init__(self, data):
         self.data = data
 
     def to_frame(self, fields=None, token_cols=None, network=None,
                  time_cols=None, units=None, sort=True, sort_by=None, **kwargs):
+        """
+        Keyword arguments:
+            fields: list of fields to keep in the dataframe
+            token_cols: list of columns holding token amounts
+            network: name of network (e.g., "polkadot", "kusama")
+            time_cols: list of columns holding timestamps
+            units: list of time units to use in pd.to_datetime() when converting
+            time_cols
+            sort: whether to sort the dataframe
+            sort_by: if sort=True, which columns to sort the dataframe by
+        """
         df = pd.json_normalize(self.data, sep="_")
         if fields is not None:
             df = df.reindex(columns=fields)
 
         if token_cols is not None:
             df[token_cols] = df[token_cols].applymap(
-                lambda x: get_token_amount(x, network), na_action="ignore")
+                lambda x: get_token_amount(x, network.lower()),
+                na_action="ignore")
+
         if time_cols is not None:
             for col, unit in zip(time_cols, units):
                 df[col] = pd.to_datetime(
